df2702ff510bbcd13bd9b851856f143dd0f2b5f6
Jackie Sun <jackiesun2@gmail.com>
2021-07-21 15:15:26 +1000

    set up user authorization sign up and sign in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,30 +1,47 @@
 class UserController < ApplicationController
+    
     def create
         @user = User.create(user_params)
-        if @user.errors.any?
-            render json: @user.errors, status: :unprocessasble_entity
-        else
-            render json: @user, status: 201
+
+        if @user.valid? 
+            puts Knock::AuthToken
+            auth_token = Knock::AuthToken.new(payload: {sub: @user.id})
+            render json: {username: @user.username, jwt: auth_token.token}, status: :created
+        else 
+            puts @user.errors.full_messages
+            render json: @user.errors, status: :unprocessable_entity
         end
     end
+
+    def sign_in 
+        @user = User.find_by_email(params[:email])
+
+        if @user && @user.authenticate(params[:password])
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {username: @user.username, jwt: auth_token.token}, status: 200
+        else 
+            render json: {error: "Incorrect Email or Password"}, status: 404
+        end
+    end
+
 
     def show
         render json: @user
     end
 
-    def update
-        @user.update(user_params)
-        if @user.errors.any? 
-            render json: @user.errors, status: :unprocessasble_entity
-        else
-            render json: @user, status: 201
-        end
-    end
+    # def update
+    #     @user.update(user_params)
+    #     if @user.errors.any? 
+    #         render json: @user.errors, status: :unprocessasble_entity
+    #     else
+    #         render json: @user, status: 201
+    #     end
+    # end
 
     private
 
     def user_params 
-        params.require(:user).permit(:first_name, :last_name, :username, :password_digest, :distance_from_work, :email, :department_code, :admin)
+        params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation, :distance_from_work, :email, :department_code, :admin)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :user
     resources :user_daily
-
+    scope '/auth' do
+      post '/sign_up', to: 'user#create' 
+      post '/sign_in', to: 'user#sign_in'
+    end
   end
 end
 
